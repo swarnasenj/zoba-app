7e17b9a10d8a058df4b3602bbb67c8632d1e9961
Swarna Sen <swarnasen56@gmail.com>
2021-12-07 10:44:59 +0600

    initial commit

--- lib/views/components/product_details.dart
+++ lib/views/components/product_details.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:zoba_app/constant/app_constant.dart';
-import 'package:zoba_app/constant/asset_path.dart';
-import 'package:zoba_app/style/color.dart';
+import '../../constant/app_constant.dart';
+import '../../constant/asset_path.dart';
+import '../../constant/rating.dart';
+import '../../style/color.dart';
 
 class ProductDetails extends StatefulWidget {
   final productItem;
@@ -17,7 +18,6 @@ class _ProductDetailsState extends State<ProductDetails> {
     {"size": "L", "isClicked": false},
     {"size": "M", "isClicked": false},
     {"size": "XL", "isClicked": false},
-    
   ];
 
   int ind = -1;
@@ -26,13 +26,15 @@ class _ProductDetailsState extends State<ProductDetails> {
   @override
   initState() {
     super.initState();
-    for (int i = 0; i < AppConstant.wishList.length; i++) {
+    for (var i = 0; i < AppConstant.wishList.length; i++) {
       if (widget.productItem["id"] == AppConstant.wishList[i]["id"]) {
         isWishList = true;
       }
     }
   }
 
+  late int _rating;
+  List<Color> colors = [Colors.black, Colors.red, Colors.indigo, Colors.teal];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,54 +57,114 @@ class _ProductDetailsState extends State<ProductDetails> {
                     child: SafeArea(
                       child: Padding(
                           padding: const EdgeInsets.symmetric(
-                              horizontal: 30, vertical: 30),
+                              horizontal: 20, vertical: 20),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              const Icon(
-                                Icons.arrow_back,
-                                size: 20,
-                                color: Colors.black,
-                              ),
-                              const Icon(
-                                Icons.shopping_bag,
-                                size: 20,
-                                color: Colors.black,
+                              IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: const Icon(
+                                    Icons.arrow_back,
+                                    size: 25,
+                                    color: Colors.black,
+                                  )),
+                              Container(
+                                margin: const EdgeInsets.only(top: 15),
+                                child: const Icon(
+                                  Icons.shopping_bag,
+                                  size: 20,
+                                  color: Colors.black,
+                                ),
                               )
                             ],
                           )),
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.only(top: 195),
+                    margin: const EdgeInsets.only(top: 225),
                     width: double.infinity,
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(25)),
                     child: Padding(
-                      padding: EdgeInsets.all(30),
+                      padding: const EdgeInsets.all(30),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text('EGP 250',
-                              style: TextStyle(color: KColor.K_sale,fontSize: 20),),
-                              Icon(
-                                isWishList
-                                    ? Icons.favorite
-                                    : Icons.favorite_outline,
-                                color: KColor.K_sale,
+                              const Text(
+                                'EGP 250',
+                                style: TextStyle(
+                                    color: KColor.K_sale, fontSize: 20),
+                              ),
+                              Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                  color: KColor.K_background,
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(50),
+                                      topRight: Radius.circular(50),
+                                      bottomLeft: Radius.circular(50),
+                                      bottomRight: Radius.circular(50)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 2,
+                                      blurRadius: 12,
+                                      offset: Offset(0, 3),
+                                    ),
+                                  ],
+                                ),
+                                child: Icon(
+                                  isWishList
+                                      ? Icons.favorite
+                                      : Icons.favorite_outline,
+                                  color: KColor.K_sale,
+                                  size: 25,
+                                ),
                               ),
                             ],
                           ),
-                          const SizedBox(height: 10,),
-                          const Text('Zarau Shoes',
-                          style: TextStyle(color: Colors.black38,fontSize: 12,fontWeight: FontWeight.bold),),
-                          const SizedBox(height: 5,),
-                          const Text('Girl\'s Sneaker Shoes',style: TextStyle(color: Colors.black,fontSize: 18),),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Zarau Shoes',
+                                  style: TextStyle(
+                                      color: Colors.black38,
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  'Girl\'s Sneaker Shoes',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(right: 92),
+                                  alignment: Alignment.centerLeft,
+                                  child: Rating((rating) {
+                                    setState(() {
+                                      _rating = rating;
+                                    });
+                                  }, 5),
+                                ),
+                              ],
+                            ),
+                          )
                         ],
                       ),
                     ),
@@ -110,26 +172,134 @@ class _ProductDetailsState extends State<ProductDetails> {
                 ],
               ),
               Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: List.generate(sizeList.length, (index) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          ind = index;
-                        });
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                            color: ind == index
-                                ? KColor.K_app
-                                : KColor.K_sale),
-                        child: Text("${sizeList[index]["size"]}", style: TextStyle(fontSize: 12),),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Text(
+                            'COLOR',
+                            style: TextStyle(color: Colors.black, fontSize: 14),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: List.generate(
+                            4,
+                            (index) => Container(
+                              margin: EdgeInsets.only(right: 10),
+                              height: 24,
+                              width: 24,
+                              decoration: BoxDecoration(
+                                  color: colors[index],
+                                  borderRadius: BorderRadius.circular(17)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          child: Text(
+                            'SIZE',
+                            style: TextStyle(color: Colors.black, fontSize: 14),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: List.generate(sizeList.length, (index) {
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  ind = index;
+                                });
+                              },
+                              child: Container(
+                                margin: EdgeInsets.only(right: 3),
+                                padding: const EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(17),
+                                    color: ind == index
+                                        ? KColor.K_app
+                                        : KColor.K_sale),
+                                child: Text(
+                                  "${sizeList[index]["size"]}",
+                                  style: const TextStyle(fontSize: 12),
+                                ),
+                              ),
+                            );
+                          }),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Products Description',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'It is a long established fact that a reader will be distracted by the readable content of a',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
                       ),
-                    );
-                  }),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {},
+                  color: KColor.K_sale,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(
+                        Icons.shopping_cart,
+                        size: 20,
+                        color: Colors.white,
+                      ),
+                      const Text(
+                        'ADD TO CART',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                      
+                    ],
+                  ),
                 ),
               ),
             ],
--- lib/views/components/products.dart
+++ lib/views/components/products.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:zoba_app/constant/app_constant.dart';
-import 'package:zoba_app/views/components/product_details.dart';
+import '../../constant/app_constant.dart';
 
-import '../../constant/asset_path.dart';
 import '../../style/color.dart';
+import 'product_details.dart';
 
 class Products extends StatefulWidget {
   const Products({Key? key}) : super(key: key);
@@ -22,95 +21,92 @@ class _ProductsState extends State<Products> {
           child: ListView(
             scrollDirection: Axis.horizontal,
             children: [
-             Row(
-              children:
-                  List.generate(AppConstant.productList.length, (int index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ProductDetails(
-                                AppConstant.productList[index])));
-                  },
-                  
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          AppConstant.wishList
-                              .add(AppConstant.productList[index]);
-          
-                          print(AppConstant.wishList);
-                        },
-                        child: Stack(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  color: KColor.K_app,
-                                  borderRadius: BorderRadius.circular(3)),
-                              height: 100,
-                              width: 100,
-                              child: Image.asset(
-                                "${AppConstant.productList[index]["image"]}",
-                              ),
-                            ),
-                            Container(
-                                padding: EdgeInsets.only(
-                                  left: 70,
+              Row(
+                children:
+                    List.generate(AppConstant.productList.length, (int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProductDetails(
+                                  AppConstant.productList[index])));
+                    },
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ProductDetails(context)));
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              AppConstant.wishList
+                                  .add(AppConstant.productList[index]);
+
+                              print(AppConstant.wishList);
+                            },
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: KColor.K_app,
+                                      borderRadius: BorderRadius.circular(3)),
+                                  height: 100,
+                                  width: 100,
+                                  child: Image.asset(
+                                    "${AppConstant.productList[index]["image"]}",
+                                  ),
                                 ),
-                                alignment: Alignment.topRight,
-                                child: IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.favorite_outline,
-                                      size: 14,
-                                    ))),
-                          ],
-                        ),
-                      ),
-                      
-                      InkWell(
-                        onTap: (){
-                           Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ProductDetails(
-                                context)));
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 5),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "${AppConstant.productList[index]["name"]}",
-                                style:
-                                    TextStyle(color: KColor.K_text, fontSize: 14),
-                              ),
-                              Text(
-                                "${AppConstant.productList[index]["category"]}",
-                                style:
-                                    TextStyle(color: KColor.K_text, fontSize: 14),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                "\$${AppConstant.productList[index]["price"]}",
-                                style:
-                                    TextStyle(color: KColor.K_sale, fontSize: 13),
-                              ),
-                            ],
+                                Container(
+                                    padding: const EdgeInsets.only(
+                                      left: 70,
+                                    ),
+                                    alignment: Alignment.topRight,
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.favorite_outline,
+                                          size: 14,
+                                        ))),
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              }),
-            ),
+                          Container(
+                            margin: const EdgeInsets.only(left: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${AppConstant.productList[index]["name"]}",
+                                  style: const TextStyle(
+                                      color: KColor.K_text, fontSize: 14),
+                                ),
+                                Text(
+                                  "${AppConstant.productList[index]["category"]}",
+                                  style: const TextStyle(
+                                      color: KColor.K_text, fontSize: 14),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "\$${AppConstant.productList[index]["price"]}",
+                                  style: const TextStyle(
+                                      color: KColor.K_sale, fontSize: 13),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
             ],
           )),
     );
